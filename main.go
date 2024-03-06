f4fb3f4cd29b376c9745ee648e64c3dd9a861471
Utkarsh <utkarshnagar2000@gmail.com>
2024-03-06 12:25:52 +0530

    publish

--- main.go
+++ main.go
@@ -4,26 +4,41 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Utkarshn10/go-rate-limiter/src/ratelimiter"
 )
 
+var ip_addresses = make(map[string]*ratelimiter.TokenBucket)
+
 func apiRequestHandler() {
-	tokenBucketRateLimiter := ratelimiter.NewTokenBucket(10, 1, nil)
 	http.HandleFunc("/limited", func(w http.ResponseWriter, r *http.Request) {
-		if tokenBucketRateLimiter.Ratelimiter(1) {
+		ip_addr := r.RemoteAddr
+
+		_, ok := ip_addresses[ip_addr]
+		if !ok {
+			ip_addresses[ip_addr] = ratelimiter.NewTokenBucket(10)
+		}
+
+		if ip_addresses[ip_addr].IsRequestAllowed() {
+			w.WriteHeader(http.StatusOK)
 			fmt.Fprint(w, "Limited")
 		} else {
-			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+			w.WriteHeader(http.StatusTooManyRequests)
+			w.Write([]byte("Too many requests!\n"))
 		}
 	})
-	http.HandleFunc("/unlimited", func(w http.ResponseWriter, r *http.Request) {
-		if tokenBucketRateLimiter.Ratelimiter(1) {
-			fmt.Fprint(w, "Unlimited")
-		} else {
-			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+
+	go func() {
+		for {
+			if len(ip_addresses) > 0 {
+				time.Sleep(time.Second)
+				for _, tb := range ip_addresses {
+					tb.Refill()
+				}
+			}
 		}
-	})
+	}()
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- src/ratelimiter/ratelimiter.go
+++ src/ratelimiter/ratelimiter.go
@@ -2,53 +2,40 @@ package ratelimiter
 
 import (
 	"math"
-	"net"
 	"time"
 )
 
-type tokenBucket struct {
-	tokens         float64
-	id             net.IP
+type TokenBucket struct {
+	tokens         int
 	refillTime     time.Time
-	maxTokens      float64
+	maxTokens      int
 	lastRefillTime time.Time
-	refillTimeRate float64
 }
 
-func NewTokenBucket(tokens float64, rate float64, ip net.IP) *tokenBucket {
-	return &tokenBucket{
+var refillRate float64 = 1
+
+func NewTokenBucket(tokens int) *TokenBucket {
+	return &TokenBucket{
 		tokens:         tokens,
 		maxTokens:      tokens,
 		lastRefillTime: time.Now(),
-		id:             ip,
-		refillTimeRate: rate,
 	}
 }
 
-// function to refill the tokenBucket
-func (tb *tokenBucket) refill() {
+// function to refill the TokenBucket
+func (tb *TokenBucket) Refill() {
 	currentTime := time.Now()
 	timeSinceLastRefill := currentTime.Sub(tb.lastRefillTime)
-	tokensToAdd := timeSinceLastRefill.Seconds() * tb.refillTimeRate
-	tb.tokens = math.Min(tb.tokens+tokensToAdd, tb.maxTokens)
+	tokensToAdd := timeSinceLastRefill.Seconds() * refillRate
+	tb.tokens = int(math.Min(float64(tb.tokens)+tokensToAdd, float64(tb.maxTokens)))
 	tb.lastRefillTime = time.Now()
 }
 
-func (tb *tokenBucket) Ratelimiter(requestNumber float64) bool {
-	// refill the bucket based on refillTime
-	tb.refill()
-	if requestNumber < tb.tokens {
-		tb.tokens -= requestNumber
+func (tb *TokenBucket) IsRequestAllowed() bool {
+	if tb.tokens > 0 {
+		tb.tokens--
 		return true
+	} else {
+		return false
 	}
-	return false
-}
-
-func main() {
-	// newTokenBucket := NewTokenBucket(10, 1)
-	// var i float64
-	// for i = 0; i < 20; i++ {
-	// 	fmt.Println("rate limit check ", i+1, newTokenBucket.ratelimiter(2))
-	// 	time.Sleep(500 * time.Millisecond)
-	// }
 }
