e509c3c6a4e843397db9c6b9b01fdfdc2a26aa69
Utkarsh <utkarshnagar2000@gmail.com>
2024-03-07 16:30:10 +0530

    added mutex

--- src/ratelimiter/ratelimiter.go
+++ src/ratelimiter/ratelimiter.go
@@ -2,6 +2,7 @@ package ratelimiter
 
 import (
 	"math"
+	"sync"
 	"time"
 )
 
@@ -10,6 +11,7 @@ type TokenBucket struct {
 	refillTime     time.Time
 	maxTokens      int
 	lastRefillTime time.Time
+	mutex          sync.Mutex
 }
 
 var refillRate float64 = 1
@@ -25,13 +27,19 @@ func NewTokenBucket(tokens int) *TokenBucket {
 // function to refill the TokenBucket
 func (tb *TokenBucket) Refill() {
 	currentTime := time.Now()
+	tb.mutex.Lock()
 	timeSinceLastRefill := currentTime.Sub(tb.lastRefillTime)
 	tokensToAdd := timeSinceLastRefill.Seconds() * refillRate
 	tb.tokens = int(math.Min(float64(tb.tokens)+tokensToAdd, float64(tb.maxTokens)))
 	tb.lastRefillTime = time.Now()
+
+	tb.mutex.Unlock()
 }
 
+// checks if the request can be made or not. If tokens are greater than 0 then return true
 func (tb *TokenBucket) IsRequestAllowed() bool {
+	tb.mutex.Lock()
+	defer tb.mutex.Unlock()
 	if tb.tokens > 0 {
 		tb.tokens--
 		return true
